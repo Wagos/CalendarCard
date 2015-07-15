0dc221eef7ede6abe5f40048bed41639f01e6ad3
Dmitry Vitkovsky <dmitry.witkowski@itrexgroup.com>
2015-07-15 22:35:32 +0300

    Initial commit

---
+++ app/src/main/java/com/wagos/calendarcard/CalendarCardPager.java
@@ -0,0 +1,19 @@
+package com.wagos.calendarcard;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by Wagos
+ */
+public class CalendarCardPager extends ViewPager{
+
+    public CalendarCardPager(Context context) {
+        super(context);
+    }
+
+    public CalendarCardPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
